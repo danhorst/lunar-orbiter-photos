665d7710ff70cfd6c56152ef8016e76c1eaa9485
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2018-06-16 14:22:06 -0400

    Traversing two layers

--- crawl.rb
+++ crawl.rb
@@ -3,13 +3,14 @@
 require 'rubygems'
 require 'mechanize'
 require 'pry'
+require 'URI'
 
 class Crawler
   IMG_HOME='https://pds-imaging.jpl.nasa.gov/data/lo/LO_1001/EXTRAS/BROWSE/'
 
   def initialize
     missions = list_folders
-    binding.pry
+    frames = list_frames(missions: missions)
   end
 
   def list_folders(url: IMG_HOME)
@@ -18,6 +19,28 @@ class Crawler
     links = page.search('#indexlist a')
     content_links = links[7..-1]
     content_links.reject { |link| link.text.empty? }
+  end
+
+  def list_frames(missions: missions)
+    frames = Hash.new
+    missions.each do |mission|
+      mission_path = path_from_anchor(mission)
+      url = content_url(mission: mission_path)
+      frame_paths = []
+      list_folders(url: url).each do |frame|
+        frame_paths << path_from_anchor(frame)
+      end
+      frames[mission_path] = frame_paths
+    end
+    frames
+  end
+
+  def path_from_anchor(anchor)
+    anchor.attribute_nodes.first.value
+  end
+
+  def content_url(mission:, frame: '')
+    URI.join(IMG_HOME, mission, frame)
   end
 end
 
