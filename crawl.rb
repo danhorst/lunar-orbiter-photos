3a8278e23a93c91d9661b7cbf29fc672a1a9ddd6
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2018-06-16 17:40:03 -0400

    Quick-and-dirty crawler; caching scraping output

--- crawl.rb
+++ crawl.rb
@@ -3,17 +3,46 @@
 require 'rubygems'
 require 'mechanize'
 require 'pry'
+require 'JSON'
 require 'URI'
 
 class Crawler
   IMG_HOME='https://pds-imaging.jpl.nasa.gov/data/lo/LO_1001/EXTRAS/BROWSE/'
 
-  def initialize
-    missions = list_folders
-    frames = list_frames(missions: missions)
+  def self.fetch_structure
+    crawler = self.new
+    frames = crawler.list_frames(missions: crawler.list_content)
+    JSON.pretty_generate(frames)
   end
 
-  def list_folders(url: IMG_HOME)
+  def self.structure_paths(input_file: 'structure.json')
+     if File.exist? input_file
+       input = File.read input_file
+       structure = JSON.parse(input)
+     else
+       structure = fetch_structure
+     end
+     frame_urls = []
+     structure.each_pair do |mission, frames|
+       frames.each do |frame|
+         frame_urls << content_url(mission: mission, frame: frame)
+       end
+     end
+     frame_urls
+  end
+
+  def self.list_images(paths: self.structure_paths)
+    crawler = self.new
+    image_urls = []
+    paths.each do |path|
+      crawler.list_content(url: path).each do |image|
+        image_urls << URI.join(path, path_from_anchor(image))
+      end
+    end
+    image_urls
+  end
+
+  def list_content(url: IMG_HOME)
     agent = Mechanize.new
     page = agent.get(url)
     links = page.search('#indexlist a')
@@ -21,13 +50,13 @@ class Crawler
     content_links.reject { |link| link.text.empty? }
   end
 
-  def list_frames(missions: missions)
+  def list_frames(missions:)
     frames = Hash.new
     missions.each do |mission|
       mission_path = path_from_anchor(mission)
       url = content_url(mission: mission_path)
       frame_paths = []
-      list_folders(url: url).each do |frame|
+      list_content(url: url).each do |frame|
         frame_paths << path_from_anchor(frame)
       end
       frames[mission_path] = frame_paths
@@ -35,13 +64,37 @@ class Crawler
     frames
   end
 
-  def path_from_anchor(anchor)
+  def list_images(frames:)
+    image_urls = []
+    frames.each_pair do |mission, frame_list|
+      frame_list.each do |frame|
+        frame_url = content_url(mission: mission, frame: frame)
+	list_content(url: frame_url).each do |image|
+	  image_path = path_from_anchor(image)
+	  image_urls << content_url(mission: mission, frame: frame, image: image_path)
+	end
+      end
+    end
+    image_urls
+  end
+
+  def self.path_from_anchor(anchor)
     anchor.attribute_nodes.first.value
   end
 
-  def content_url(mission:, frame: '')
-    URI.join(IMG_HOME, mission, frame)
+  def path_from_anchor(anchor)
+    self.class.path_from(anchor)
+  end
+
+  def self.content_url(mission:, frame: '', image: '')
+    URI.join(IMG_HOME, mission, frame, image)
+  end
+
+  def content_url(**args)
+    self.class.content_url(**args)
   end
 end
 
-Crawler.new
+#Crawler.fetch_structure
+#puts Crawler.structure_paths
+puts Crawler.list_images
